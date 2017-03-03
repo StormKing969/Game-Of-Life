85c89688c5c535666655dd688449a4c5b57f9f00
wasajana <wijesinghesaj@gmail.com>
2017-03-03 13:18:54 -0500

    Update GameOfLife.py

--- GameOfLife.py
+++ GameOfLife.py
@@ -64,5 +64,97 @@ def innerReverse(A):
                 A[row][col]=0
     return A
 ############################################################
-    
-
+#part8
+def countNeighbors(row, col, A):
+    '''This function determines the number of alive neighbors
+       around the cell in a particular location.'''
+    liveCounter = 0
+    #if statements below this comment check for neighbors
+    #diagonal to the cell.
+    if A[row-1][col-1] == 1:
+        liveCounter += 1
+    if A[row+1][col+1] == 1:
+        liveCounter += 1
+    if A[row-1][col+1] == 1:
+        liveCounter += 1
+    if A[row+1][col-1] == 1:
+        liveCounter += 1
+    #if statements below this comment check for neighbors
+    #above, below, left and right to the cell. 
+    if A[row-1][col] == 1:
+        liveCounter += 1
+    if A[row+1][col] == 1:
+        liveCounter += 1
+    if A[row][col-1] == 1:
+        liveCounter += 1
+    if A[row][col+1] == 1:
+        liveCounter += 1
+    return liveCounter
+############################################################
+def next_life_generation(A):
+    '''makes a copy of A and then advanced one generation
+       of Conways's game of life within the *inner cels* of
+       that copy. The outer edge is always zero.'''
+    newA = deepcopy(A)
+    for row in range(1, len(newA)-1):
+        for col in range(1, len(newA[0])-1):
+             if countNeighbors(row, col, A) == 3:
+                 newA[row][col] = 1
+             if countNeighbors(row, col, A) < 2:
+                 newA[row][col] = 0
+             if countNeighbors(row, col, A) > 3:
+                 newA[row][col] = 0
+    return newA
+############################################################
+def interact():
+#'''Interact with a use to play Conway's Game of Life.'''
+    line = raw_input("Enter Command:")
+    line = line.replace(" ", "")
+    while line[0] != "q":
+        command = line[0]
+        if command == 'n':
+            evalA = eval(line[1:]) 
+            x = evalA[0] 
+            y = evalA[1] 
+            A = createBoard(x,y)
+        elif command == 'i':
+            evalA2 = eval(line[1:])
+            count = 0 
+            for elements in range(len(evalA2)): 
+                x = evalA2[count][0] 
+                y = evalA2[count][1]  
+                count += 1
+                A[x][y] = 1
+        elif command == 'p':
+            printBoard(A)
+        elif command == 'd':
+            copyA = copy(A)
+            name = [len(x) for x in copyA]
+            w = name[0]
+            h = len(copyA)
+            for row in range(h):
+                for col in range(w):
+                    if A[row][col] == 1:
+                        copyA[row][col] = "@"
+                    elif A[row][col] == 0:
+                        copyA[row][col] = " "
+            printBoard(copyA)
+        elif command == 'r':
+            nextGen = line[1:]
+            while nextGen != 0:
+                A = next_life_generation(A)
+                finalA = enter(w, h, A)
+                evaluate -= 1 #decrement 
+                printBoard(finalA)
+        elif command == 'h':
+            print "n-enter height width for the board"
+            print "i-start life"
+            print "p-print the board)"
+            print "d-print the board after replacing 0s and 1s"
+            print "r-show different generations for n times"
+            print "h-help menu"
+            print "q-quit"
+        line = raw_input("Enter Command: ")
+        line = line.replace(" ", "")
+    print "Life is over\n"
+############################################################
